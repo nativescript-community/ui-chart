bf173ae5d5ce829204900a713ef2f453ed51170b
Martin Guillon <martin.guillon@akylas.fr>
2020-11-06 22:07:09 +0100

    chore: cleanup

--- src/charting/utils/TransformerHorizontalBarChart.ts
+++ src/charting/utils/TransformerHorizontalBarChart.ts
@@ -2,27 +2,27 @@ import { Transformer } from './Transformer';
 
 /**
  * Transformer class for the HorizontalBarChart.
- * 
+ *
  * @author Philipp Jahoda
  */
 export class TransformerHorizontalBarChart extends Transformer {
     /**
      * Prepares the matrix that contains all offsets.
-     * 
+     *
      * @param inverted
      */
     public prepareMatrixOffset(inverted) {
-
         this.mMatrixOffset.reset();
 
         // offset.postTranslate(mOffsetLeft, getHeight() - this.mOffsetBottom);
 
         if (!inverted) {
             this.mMatrixOffset.postTranslate(this.mViewPortHandler.offsetLeft(), this.mViewPortHandler.getChartHeight() - this.mViewPortHandler.offsetBottom());
-        }
-        else {
-            this.mMatrixOffset.setTranslate(-(this.mViewPortHandler.getChartWidth() - this.mViewPortHandler.offsetRight()), 
-                this.mViewPortHandler.getChartHeight() - this.mViewPortHandler.offsetBottom());
+        } else {
+            this.mMatrixOffset.setTranslate(
+                -(this.mViewPortHandler.getChartWidth() - this.mViewPortHandler.offsetRight()),
+                this.mViewPortHandler.getChartHeight() - this.mViewPortHandler.offsetBottom()
+            );
             this.mMatrixOffset.postScale(-1.0, 1.0);
         }
 
