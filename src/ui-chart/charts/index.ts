b56a87abf7a287d98bf7597bba6ec28f7ea88f19
farfromrefug <dev@akylas.fr>
2024-04-25 16:54:20 +0200

    fix: regression fix with barel import

---
+++ src/ui-chart/charts/index.ts
@@ -0,0 +1,9 @@
+export { BarChart } from './BarChart';
+export { HorizontalBarChart } from './HorizontalBarChart';
+export { LineChart } from './LineChart';
+export { PieChart } from './PieChart';
+export { RadarChart } from './RadarChart';
+export { ScatterChart } from './ScatterChart';
+export { BubbleChart } from './BubbleChart';
+export { CandleStickChart } from './CandleStickChart';
+export { CombinedChart } from './CombinedChart';
